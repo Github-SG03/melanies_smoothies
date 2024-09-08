c645b95c0e41a20b2edf7e154e4066f90738ee02
Github-SG03 <sgs.shivam99@outlook.com>
2024-09-08 11:25:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #convert the snowspark dataframe to pandas dataframe so that we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose upto five ingredients:',
@@ -42,8 +42,8 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         #A Strange-Looking Statement That Will Get Us the "Search On" Value
-        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
        
         #Use Our fruit_chosen Variable in the API Call
         st.subheader=(fruit_chosen + 'Nutrition Information')
