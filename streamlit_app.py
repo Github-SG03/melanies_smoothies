4100bfba3a563851ca64b1c1356c895c3adfb62b
Github-SG03 <sgs.shivam99@outlook.com>
2024-09-18 23:19:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ if ingredients_list:
         session.sql(my_insert_stmts).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
